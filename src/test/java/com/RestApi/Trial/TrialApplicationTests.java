3a9537d5c96079f1798539e4018b04a0fc42ad1e
Elizabeth Mathew <62603419+Elizabeth-Mathew1@users.noreply.github.com>
2024-07-08 14:25:07 +0530

    adds base class and tests

---
+++ src/test/java/com/RestApi/Trial/TrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestApi.Trial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
